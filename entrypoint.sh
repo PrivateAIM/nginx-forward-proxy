810bfe667041dc9f9047eb03213dba8d85c09117
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2025-05-20 13:32:42 +0200

    fix: avoid race condition when reading and writing to same file in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-envsubst < /usr/local/nginx/conf/nginx.conf | tee /usr/local/nginx/conf/nginx.conf >/dev/null
+envsubst < /usr/local/nginx/conf/nginx.conf | tee /tmp/nginx.conf >/dev/null && \
+    mv /tmp/nginx.conf /usr/local/nginx/conf/nginx.conf
+
 exec /usr/sbin/nginx "${@}"
