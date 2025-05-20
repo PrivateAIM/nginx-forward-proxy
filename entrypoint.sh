3a6acdc9535b3ff50ece221d380d98e945cb5294
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2025-05-20 13:43:08 +0200

    fix: expand only `PROXY_ADDRESS` in nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-envsubst < /usr/local/nginx/conf/nginx.conf | tee /tmp/nginx.conf >/dev/null && \
+
+# SC2016 states that variables are not expanded in single quotes, but this is intended here.
+# shellcheck disable=SC2016
+envsubst '${PROXY_ADDRESS}' < /usr/local/nginx/conf/nginx.conf | tee /tmp/nginx.conf >/dev/null && \
     mv /tmp/nginx.conf /usr/local/nginx/conf/nginx.conf
 
 exec /usr/sbin/nginx "${@}"
