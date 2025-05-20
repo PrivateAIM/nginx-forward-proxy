cc7e56a1ffe6d13b24d70f5aa24f4af896e022c8
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2025-05-20 14:31:05 +0200

    refactor: remove entrypoint script

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# SC2016 states that variables are not expanded in single quotes, but this is intended here.
-# shellcheck disable=SC2016
-envsubst '${PROXY_ADDRESS}' < /usr/local/nginx/conf/nginx.conf | tee /tmp/nginx.conf >/dev/null && \
-    mv /tmp/nginx.conf /usr/local/nginx/conf/nginx.conf
-
-exec /usr/sbin/nginx "${@}"
